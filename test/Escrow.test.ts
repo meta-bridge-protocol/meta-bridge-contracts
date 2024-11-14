c797d0542c2c5eef60d4af5e465f05f6ce23badd
abbasnosrati <abbas.nosrati68@gmail.com>
2024-11-14 15:30:38 +0330

    update escrow test

--- test/Escrow.test.ts
+++ test/Escrow.test.ts
@@ -14,9 +14,11 @@ const ILzEndpointV2 = require("../artifacts/contracts/interfaces/ILzEndpointV2.s
 describe("Escrow", () => {
   let escrow: Escrow;
   let owner: SignerWithAddress;
+  let treasure: SignerWithAddress;
   let depositor: SignerWithAddress;
   let withdrawer: SignerWithAddress;
   let assetManager: SignerWithAddress;
+  let user: SignerWithAddress;
   let mbToken: MBToken;
   let gateway: Gateway;
   let lzSendLib: Address;
@@ -25,10 +27,11 @@ describe("Escrow", () => {
   let nativeToken: TestToken;
 
   const initialThreshold = ethers.utils.parseEther("100");
-  const depositAmount = ethers.utils.parseEther("50");
+  const escrowMintAmount = ethers.utils.parseEther("150");
 
   before(async () => {
-    [owner, depositor, withdrawer, assetManager] = await ethers.getSigners();
+    [owner, depositor, withdrawer, assetManager, user, treasure] =
+      await ethers.getSigners();
   });
 
   const deployMbToken = async () => {
@@ -76,7 +79,11 @@ describe("Escrow", () => {
     escrow = await EscrowFactory.deploy();
 
     // Initializing the Escrow contract
-    await escrow.initialize(gateway.address, owner.address, initialThreshold);
+    await escrow.initialize(
+      gateway.address,
+      treasure.address,
+      initialThreshold
+    );
 
     // Granting roles
     await escrow.grantRole(await escrow.DEPOSITOR_ROLE(), depositor.address);
@@ -85,29 +92,113 @@ describe("Escrow", () => {
       await escrow.ASSET_MANAGER_ROLE(),
       assetManager.address
     );
+
+    await nativeToken.connect(owner).mint(escrow.address, escrowMintAmount);
+    await nativeToken.connect(owner).approve(gateway.address, escrowMintAmount);
+
+    expect(await nativeToken.balanceOf(escrow.address)).to.be.equal(
+      escrowMintAmount
+    );
+
+    expect(await nativeToken.balanceOf(gateway.address)).to.be.equal(0);
   });
 
   describe("Deployment", () => {
     it("should set the correct initial parameters", async () => {
       expect(await escrow.nativeTokenAddress()).to.equal(nativeToken.address);
-      expect(await escrow.treasureAddress()).to.equal(owner.address);
+      expect(await escrow.treasureAddress()).to.equal(treasure.address);
       expect(await escrow.thresholdAmount()).to.equal(initialThreshold);
     });
   });
 
   describe("Deposit Functionality", () => {
-    it("should allow the depositor to deposit tokens if below threshold", async () => {
-      await nativeToken.mint(escrow.address, depositAmount);
-      await nativeToken.approve(gateway.address, depositAmount);
+    it("should not allow the non-depositor to deposit", async () => {
+      expect(await nativeToken.balanceOf(gateway.address)).to.be.equal("0");
+
+      await expect(
+        escrow.connect(user).depositToGateway()
+      ).to.revertedWithCustomError(escrow, "AccessControlUnauthorizedAccount");
+
+      expect(await nativeToken.balanceOf(gateway.address)).to.be.equal(0);
+    });
+
+    it("should allow the depositor to deposit tokens if below threshold and required amount less than escrowBalance", async () => {
+      const gatewayBalance = await nativeToken.balanceOf(gateway.address);
+      expect(gatewayBalance).to.be.equal(0);
+
+      expect(await nativeToken.balanceOf(treasure.address)).to.be.equal(0);
+
+      const escrowBalance = await nativeToken.balanceOf(escrow.address);
+      expect(escrowBalance).to.be.equal(escrowMintAmount);
+
+      const requiredAmount = initialThreshold.sub(gatewayBalance);
+
+      expect(requiredAmount).lt(escrowBalance);
+
+      expect(await gateway.deposits(escrow.address)).to.be.equal(0);
+
       await expect(escrow.connect(depositor).depositToGateway())
         .to.emit(escrow, "DepositToGateway")
-        .withArgs(depositAmount, initialThreshold);
+        .withArgs(requiredAmount, initialThreshold);
+
+      expect(await gateway.deposits(escrow.address)).to.be.equal(
+        requiredAmount
+      );
+
+      expect(await nativeToken.balanceOf(escrow.address)).to.be.equal(
+        escrowBalance.sub(requiredAmount)
+      );
+
+      expect(await nativeToken.balanceOf(gateway.address)).to.be.equal(
+        requiredAmount
+      );
+
+      expect(await nativeToken.balanceOf(treasure.address)).to.be.equal(0);
+    });
+
+    it("should allow the depositor to deposit tokens if below threshold and escrowBalance less than required amount", async () => {
+      const withdrawAmount = ethers.utils.parseEther("100");
+      const gatewayBalance = await nativeToken.balanceOf(gateway.address);
+      expect(gatewayBalance).to.be.equal(0);
+
+      expect(await nativeToken.balanceOf(treasure.address)).to.be.equal(0);
+
+      let escrowBalance = await nativeToken.balanceOf(escrow.address);
+      expect(escrowBalance).to.be.equal(escrowMintAmount);
+
+      await escrow
+        .connect(assetManager)
+        .withdrawERC20(nativeToken.address, withdrawAmount);
+
+      escrowBalance = await nativeToken.balanceOf(escrow.address);
+      expect(escrowBalance).to.be.equal(escrowMintAmount.sub(withdrawAmount));
+
+      const requiredAmount = initialThreshold.sub(gatewayBalance);
+
+      expect(escrowBalance).lt(requiredAmount);
+
+      expect(await gateway.deposits(escrow.address)).to.be.equal(0);
+
+      await expect(escrow.connect(depositor).depositToGateway())
+        .to.emit(escrow, "DepositToGateway")
+        .withArgs(escrowBalance, initialThreshold);
+
+      expect(await gateway.deposits(escrow.address)).to.be.equal(escrowBalance);
+
+      expect(await nativeToken.balanceOf(escrow.address)).to.be.equal(0);
+
+      expect(await nativeToken.balanceOf(gateway.address)).to.be.equal(
+        escrowBalance
+      );
+      expect(await nativeToken.balanceOf(treasure.address)).to.be.equal(
+        withdrawAmount
+      );
     });
 
     it("should revert if the gateway balance exceeds the threshold", async () => {
-      await nativeToken.mint(escrow.address, depositAmount);
-      await nativeToken.approve(gateway.address, depositAmount);
-      await nativeToken.mint(gateway.address, depositAmount.mul(2));
+      await nativeToken
+        .connect(owner)
+        .mint(gateway.address, escrowMintAmount.mul(2));
       await expect(
         escrow.connect(depositor).depositToGateway()
       ).to.be.revertedWith("Escrow: Gateway balance exceeds the threshold");
@@ -115,21 +206,98 @@ describe("Escrow", () => {
   });
 
   describe("Withdraw Functionality", () => {
-    const withdrawAmount = ethers.utils.parseUnits("10", 18);
+    let gatewayBalance = null;
+    let escrowBalance = null;
+    let escrowBalanceAfterDeposit = null;
+    let requiredAmount = null;
+
+    beforeEach(async () => {
+      gatewayBalance = await nativeToken.balanceOf(gateway.address);
+      expect(gatewayBalance).to.be.equal(0);
+
+      expect(await nativeToken.balanceOf(treasure.address)).to.be.equal(0);
+
+      escrowBalance = await nativeToken.balanceOf(escrow.address);
+      expect(escrowBalance).to.be.equal(escrowMintAmount);
+
+      requiredAmount = initialThreshold.sub(gatewayBalance);
+
+      expect(requiredAmount).lt(escrowBalance);
+
+      //Deposit to gateway
+      await expect(escrow.connect(depositor).depositToGateway())
+        .to.emit(escrow, "DepositToGateway")
+        .withArgs(requiredAmount, initialThreshold);
+
+      expect(await nativeToken.balanceOf(escrow.address)).to.be.equal(
+        escrowBalance.sub(requiredAmount)
+      );
+
+      escrowBalanceAfterDeposit = await nativeToken.balanceOf(escrow.address);
+
+      expect(await nativeToken.balanceOf(gateway.address)).to.be.equal(
+        requiredAmount
+      );
+    });
+
+    it("should not allow the withdrawer to withdraw tokens", async () => {
+      await expect(
+        escrow.connect(user).withdrawFromGateway()
+      ).to.be.revertedWithCustomError(
+        escrow,
+        "AccessControlUnauthorizedAccount"
+      );
+    });
+
     it("should allow the withdrawer to withdraw tokens if above threshold", async () => {
-      await nativeToken.mint(escrow.address, withdrawAmount);
-      await nativeToken.approve(gateway.address, withdrawAmount);
-      await escrow.connect(depositor).depositToGateway();
-      await nativeToken.mint(gateway.address, initialThreshold);
+      await nativeToken.connect(owner).mint(gateway.address, initialThreshold);
+
+      gatewayBalance = await nativeToken.balanceOf(gateway.address);
+
+      expect(gatewayBalance).to.be.equal(requiredAmount.add(initialThreshold));
+
+      expect(await gateway.deposits(escrow.address)).to.be.equal(
+        requiredAmount
+      );
+
+      expect(await nativeToken.balanceOf(treasure.address)).to.be.equal(0);
+
+      const withdrawerRequiredAmount = gatewayBalance.sub(initialThreshold);
       await expect(escrow.connect(withdrawer).withdrawFromGateway())
         .to.emit(escrow, "WithdrawFromGateway")
-        .withArgs(withdrawAmount, initialThreshold);
+        .withArgs(withdrawerRequiredAmount, initialThreshold);
+
+      expect(await nativeToken.balanceOf(escrow.address)).to.be.equal(
+        escrowBalanceAfterDeposit.add(withdrawerRequiredAmount)
+      );
+
+      expect(await gateway.deposits(escrow.address)).to.be.equal(
+        requiredAmount.sub(withdrawerRequiredAmount)
+      );
+
+      expect(await nativeToken.balanceOf(treasure.address)).to.be.equal(0);
+    });
+
+    it("should revert if user(escrow) INSUFFICIENT_USER_BALANCE", async () => {
+      await nativeToken
+        .connect(owner)
+        .mint(gateway.address, initialThreshold.mul(2));
+      gatewayBalance = await nativeToken.balanceOf(gateway.address);
+
+      const escrowDepositBalance = await gateway.deposits(escrow.address);
+
+      expect(escrowDepositBalance).lt(gatewayBalance);
+
+      await expect(
+        escrow.connect(withdrawer).withdrawFromGateway()
+      ).to.be.revertedWith("Gateway: INSUFFICIENT_USER_BALANCE");
+
+      expect(await nativeToken.balanceOf(escrow.address)).to.be.equal(
+        escrowBalanceAfterDeposit
+      );
     });
 
     it("should revert if the gateway balance is below the threshold", async () => {
-      await nativeToken.mint(escrow.address, withdrawAmount);
-      await nativeToken.approve(gateway.address, withdrawAmount);
-      await escrow.connect(depositor).depositToGateway();
       await expect(
         escrow.connect(withdrawer).withdrawFromGateway()
       ).to.be.revertedWith("Escrow: Gateway balance is below the threshold");
@@ -139,15 +307,26 @@ describe("Escrow", () => {
   describe("Threshold Management", () => {
     const newThreshold = ethers.utils.parseEther("200");
     it("should allow the admin to set a new threshold amount", async () => {
+      expect(await escrow.thresholdAmount()).to.equal(initialThreshold);
+
       await expect(escrow.connect(owner).setThresholdAmount(newThreshold))
         .to.emit(escrow, "SetThresholdAmount")
         .withArgs(newThreshold);
+
       expect(await escrow.thresholdAmount()).to.equal(newThreshold);
     });
 
     it("should revert if a non-admin tries to set a new threshold", async () => {
-      await expect(escrow.connect(depositor).setThresholdAmount(newThreshold))
-        .to.be.reverted;
+      expect(await escrow.thresholdAmount()).to.equal(initialThreshold);
+
+      await expect(
+        escrow.connect(depositor).setThresholdAmount(newThreshold)
+      ).to.be.revertedWithCustomError(
+        escrow,
+        "AccessControlUnauthorizedAccount"
+      );
+
+      expect(await escrow.thresholdAmount()).to.equal(initialThreshold);
     });
   });
 
@@ -157,7 +336,7 @@ describe("Escrow", () => {
 
       await expect(escrow.connect(owner).setTreasureAddress(newTreasureAddress))
         .to.emit(escrow, "TreasureChanged")
-        .withArgs(owner.address, newTreasureAddress);
+        .withArgs(treasure.address, newTreasureAddress);
 
       expect(await escrow.treasureAddress()).to.equal(newTreasureAddress);
     });
@@ -166,12 +345,15 @@ describe("Escrow", () => {
       const newTreasureAddress = ethers.Wallet.createRandom().address;
       await expect(
         escrow.connect(depositor).setTreasureAddress(newTreasureAddress)
-      ).to.be.reverted;
+      ).to.be.revertedWithCustomError(
+        escrow,
+        "AccessControlUnauthorizedAccount"
+      );
     });
 
+    //zero address
     it("should revert when setting treasure address to address(0)", async function () {
-      await expect(escrow.setTreasureAddress(ethers.constants.AddressZero)).not
-        .to.be;
+      await expect(escrow.setTreasureAddress(ethers.constants.AddressZero));
     });
   });
 
@@ -179,7 +361,15 @@ describe("Escrow", () => {
     it("should allow the asset manager to withdraw ERC20 tokens", async () => {
       const withdrawAmount = ethers.utils.parseEther("10");
 
-      await nativeToken.mint(escrow.address, withdrawAmount);
+      const treasureBalanceBeforeWithdraw = await nativeToken.balanceOf(
+        treasure.address
+      );
+
+      expect(treasureBalanceBeforeWithdraw).to.be.equal(0);
+
+      const escrowBalance = await nativeToken.balanceOf(escrow.address);
+
+      expect(escrowBalance).to.be.equal(escrowMintAmount);
 
       await expect(
         escrow
@@ -187,7 +377,14 @@ describe("Escrow", () => {
           .withdrawERC20(nativeToken.address, withdrawAmount)
       )
         .to.emit(escrow, "WithdrawERC20")
-        .withArgs(nativeToken.address, owner.address, withdrawAmount);
+        .withArgs(nativeToken.address, treasure.address, withdrawAmount);
+
+      expect(await nativeToken.balanceOf(escrow.address)).to.be.equal(
+        escrowBalance.sub(withdrawAmount)
+      );
+      expect(await nativeToken.balanceOf(treasure.address)).to.be.equal(
+        treasureBalanceBeforeWithdraw.add(withdrawAmount)
+      );
     });
 
     it("should revert if a non-asset manager tries to withdraw ERC20 tokens", async () => {
@@ -195,25 +392,10 @@ describe("Escrow", () => {
         escrow
           .connect(depositor)
           .withdrawERC20(nativeToken.address, ethers.utils.parseEther("10"))
-      ).to.be.reverted;
-    });
-  });
-
-  describe("AccessControl roles", () => {
-    it("should revert when non-admin tries to set threshold", async function () {
-      await expect(
-        escrow
-          .connect(depositor)
-          .setThresholdAmount(ethers.utils.parseEther("100"))
-      ).to.be.reverted;
-    });
-
-    it("should allow admin to set threshold", async function () {
-      await expect(
-        escrow.connect(owner).setThresholdAmount(ethers.utils.parseEther("100"))
-      )
-        .to.emit(escrow, "SetThresholdAmount")
-        .withArgs(ethers.utils.parseEther("100"));
+      ).to.be.revertedWithCustomError(
+        escrow,
+        "AccessControlUnauthorizedAccount"
+      );
     });
   });
 });
