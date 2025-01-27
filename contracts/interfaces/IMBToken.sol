94125b3cc47676f1a1bf26993f955d92372a379f
Shayan Shiravani <shayan6959@gmail.com>
2025-01-27 10:33:33 +0330

    IMBToken

--- contracts/interfaces/IMBToken.sol
+++ contracts/interfaces/IMBToken.sol
@@ -1,17 +1,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {ILayerZeroEndpointV2} from "@layerzerolabs/lz-evm-oapp-v2/contracts/oapp/interfaces/IOAppCore.sol";
 import {MessagingFee, SendParam, IOFT} from "@layerzerolabs/lz-evm-oapp-v2/contracts/oft/interfaces/IOFT.sol";
 
-interface IMBToken is IOFT {
+interface IMBToken is IOFT, IERC20 {
     function mint(address to, uint256 amount) external;
 
     function burnFrom(address from, uint256 amount) external;
 
     function gateway() external returns (address);
-
-    function approve(address spender, uint256 value) external returns (bool);
-
-    function transfer(address to, uint256 value) external returns (bool);
 }
