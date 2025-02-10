fa473407926ddd3b343f09ea8cefb88cd30ea90c
Shayan Shiravani <shayan6959@gmail.com>
2025-02-10 11:36:28 +0330

    scripts

--- scripts/deploy-gateway.js
+++ scripts/deploy-gateway.js
@@ -3,11 +3,11 @@ import hre, { ethers } from "hardhat";
 async function deployGatewayContract() {
   const params = [
     "0x3F85D3F9390b8Aee4292D2D543Cec166B8B55aDD",
-    "0xe094C430903C3Fe539D639123e94cE06D1feCB4d",
+    "0x99E7bD153585368c975FcFcdD2cCF0B5172963Ad",
     "0x8D4aE54e70614c21162A0faF1e0273492CC0078a",
   ];
 
-  const gateway = await ethers.deployContract("Gateway", params);
+  const gateway = await ethers.deployContract("contracts/Gateway.sol:Gateway", params);
 
   await gateway.deployed();
 
@@ -17,6 +17,7 @@ async function deployGatewayContract() {
     await hre.run("verify:verify", {
       address: gateway.address,
       constructorArguments: params,
+      contract: "contracts/Gateway.sol:Gateway"
     });
   } catch (e) {
     console.log(e);
