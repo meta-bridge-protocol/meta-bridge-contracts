682c7539daa19538afd81972f2da99ce2428c26c
Shayan Shiravani <shayan6959@gmail.com>
2025-06-16 14:49:26 +0330

    Separate deployers

--- contracts/MetaBridgeFactory.sol
+++ contracts/MetaBridgeFactory.sol
@@ -39,60 +39,6 @@ contract MetaBridgeFactory is Ownable {
         address _bridgeTreasury,
         bool _isBurnable
     ) external {
-        _listToken(_tokenId, _nativeToken, _bridgeTreasury, _isBurnable);
-    }
-
-    /**
-     * @notice List non-existing tokens on MetaBridge. It will automatically create and deploy the token.
-     * Token will be a mintable/burnable ERC20 token.
-     * @param _tokenId The unique token ID used in the bridge; it must be globally unique across all bridge contracts.
-     * @param _maxSupply The max supply of the token on current chain
-     * @param _name The name of the token
-     * @param _symbol The symbol of the token
-     * @param _bridgeTreasury The treasury address for depositing tokens instead of burning them during
-     *  the bridging process. We might do that in the main chain or when the token is not burnable
-     */
-    function ListNewToken(
-        uint256 _tokenId,
-        uint256 _maxSupply,
-        string memory _name,
-        string memory _symbol,
-        address _bridgeTreasury
-    ) external {
-        NativeToken nativeToken = new NativeToken(
-            _maxSupply,
-            _name,
-            _symbol,
-            msg.sender
-        );
-
-        emit NativeTokenCreated(msg.sender, address(nativeToken));
-
-        _listToken(_tokenId, address(nativeToken), _bridgeTreasury, true);
-    }
-
-    /**
-     *
-     * @param _lzBridge The address of the bridge contract
-     */
-    function setLzBridge(address _lzBridge) external onlyOwner {
-        lzBridge = _lzBridge;
-    }
-
-    /**
-     *
-     * @param _oApp The address of the LayerZero OApp used as the message-passing tool in the bridge.
-     */
-    function setOApp(address _oApp) external onlyOwner {
-        mbOApp = _oApp;
-    }
-
-    function _listToken(
-        uint256 _tokenId,
-        address _nativeToken,
-        address _bridgeTreasury,
-        bool _isBurnable
-    ) internal {
         string memory tokenName = ERC20(_nativeToken).name();
         string memory tokenSymbol = ERC20(_nativeToken).symbol();
 
@@ -122,5 +68,21 @@ contract MetaBridgeFactory is Ownable {
         emit MBTokenCreated(msg.sender, address(mbToken));
         emit GatewayCreated(msg.sender, address(gateway));
         emit TokenListed(_nativeToken, address(mbToken), _tokenId);
+    }
+
+    /**
+     *
+     * @param _lzBridge The address of the bridge contract
+     */
+    function setLzBridge(address _lzBridge) external onlyOwner {
+        lzBridge = _lzBridge;
+    }
+
+    /**
+     *
+     * @param _oApp The address of the LayerZero OApp used as the message-passing tool in the bridge.
+     */
+    function setOApp(address _oApp) external onlyOwner {
+        mbOApp = _oApp;
     }
 }
