2bb42b80254ce7f2c97dd07bcbebd1df950922d4
Shayan Shiravani <shayan6959@gmail.com>
2025-07-29 12:59:58 +0330

    Deus mbToken

--- contracts/MetaDEUS.sol
+++ contracts/MetaDEUS.sol
@@ -4,9 +4,5 @@ pragma solidity ^0.8.20;
 import {MBToken} from "./MBToken.sol";
 
 contract MetaDEUS is MBToken {
-    constructor(
-        address _layerZeroEndpoint, // local endpoint address
-        address _owner, // token owner used as a delegate in LayerZero Endpoint
-        address _mbOApp
-    ) MBToken("MetaDEUS", "mbDEUS", _mbOApp) {}
+    constructor(address _mbOApp) MBToken("MetaDEUS", "mbDEUS", _mbOApp) {}
 }
